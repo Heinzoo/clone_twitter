e535d87858f5f2fae11d9b18a96bb52311d99f23
Heinzoo <enzoquiomaster.com>
2023-06-30 07:20:19 -0400

    index and show tweet

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+25.times do |i|
+  Tweet.create(
+    username: 'enzo_portales',
+    description: Faker::Lorem.paragraph_by_chars(number: 140, supplemental: false)
+  )
+end
